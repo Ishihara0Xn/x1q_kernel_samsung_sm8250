5576896c6a14446b679fc2e6f0e94dddaffff7b2
Mohamedfullhd <aaamohamed312@gmail.com>
2025-05-08 20:37:46 +0300

    Build: We don't need that

--- include/linux/xattr.h
+++ include/linux/xattr.h
@@ -17,6 +17,7 @@
 #include <linux/spinlock.h>
 #include <uapi/linux/xattr.h>
 
+#define XATTR_NAME_MAX   255       /* # chars in an extended attribute name */
 #define XATTR_SIZE_MAX 65536       /* size of an extended attribute value (64k) */
 #define XATTR_LIST_MAX 65536       /* size of extended attribute namelist (64k) */
 
