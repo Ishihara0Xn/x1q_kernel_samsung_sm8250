62dd8b8536a63395e8c03f94de7c8dcc3610c915
Mohamedfullhd <aaamohamed312@gmail.com>
2025-05-08 17:45:31 +0300

    build_kernel: add defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -14,7 +14,7 @@ git clone https://gitlab.com/LeCmnGend/clang.git -b clang-18 --depth=1 $(pwd)/pr
 
 # change DEFCONFIG to you are defconfig name or device codename
 
-DEFCONFIG="vendor/samsung/x1q_defconfig"
+DEFCONFIG="vendor/x1q_kor_singlex_defconfig"
 
 # you can set you name or host name(optional)
 
